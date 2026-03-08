77d0b9c7f4cf5c447dc9460cbd6f896e5145594c
unknown <josephgenescar@gmail.com>
2026-03-08 13:17:44 -0400

    m

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,12 +1,16 @@
 const ImageKit = require('imagekit');
 
 exports.handler = async (event, context) => {
+  // Rekipere fileName si li voye l, sinon sèvi ak yon default
+  const { fileName } = event.queryStringParameters || {};
+
   const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
   });
 
+  // ImageKit bezwen menm kòd la chak fwa
   const authenticationParameters = imagekit.getAuthenticationParameters();
 
   return {
@@ -14,6 +18,7 @@ exports.handler = async (event, context) => {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(authenticationParameters),
   };
