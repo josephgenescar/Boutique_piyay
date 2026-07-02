bf21dc591719fb5009375e2fec732322b429e5e8
unknown <josephgenescar@gmail.com>
2026-07-02 16:52:57 -0400

    b

--- netlify/functions/push-registration.js
+++ netlify/functions/push-registration.js
@@ -1,7 +1,7 @@
 const ws = require('ws');
 const { createClient } = require("@supabase/supabase-js");
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_URL = process.env.SUPABASE_URL || 'https://letyferfjpxmstohvgcj.supabase.co';
 const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = SUPABASE_URL && SUPABASE_KEY
   ? createClient(SUPABASE_URL, SUPABASE_KEY, { transport: ws })
